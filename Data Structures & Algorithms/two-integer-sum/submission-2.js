2336a6bd3aee0dbca85ee6cd358151564ca9815f
Rathna Chinthalapalli <rathna.ganjigunta@gmail.com>
2026-09-08 14:46:48 -0700

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        for( let i = 0; i < nums.length; i++){
+            let comp = target - nums[i];
+            for(let j = i + 1; j < nums.length; j++){
+                if(nums[j] === comp){
+                    return [i, j]
+                }
+            }
+  
+        }
+    }
+}
